b58136f91d8143b6e73d637a09cee528c98f4b09
Gustavo Silva <guhenriquels@gmail.com>
2022-10-20 22:31:48 -0300

    Created test_view_loads_template_is_correct

--- customers/tests/test_customers.py
+++ customers/tests/test_customers.py
@@ -1,10 +1,11 @@
 from unittest import TestCase
 
 from customers import views
+from django.test import TestCase as DjangoTestCase
 from django.urls import resolve, reverse
 
 
-class TestCustomers(TestCase):
+class TestCustomersUnittestTestCase(TestCase):
     def test_url_home_is_correct(self):
         url = reverse('customers:home')
         self.assertEqual('/', url)
@@ -12,3 +13,9 @@ class TestCustomers(TestCase):
     def test_url_home_loads_view_correct(self):
         response = resolve(reverse('customers:home'))
         self.assertEqual(response.func, views.home)
+
+
+class TestCustomersDjangoTestCase(DjangoTestCase):
+    def test_view_loads_template_is_correct(self):
+        response = self.client.get(reverse('customers:home'))
+        self.assertTemplateUsed(response, 'customers/pages/home.html')
--- customers/views.py
+++ customers/views.py
@@ -1,6 +1,5 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse()
+    return render(request, 'customers/pages/home.html')
